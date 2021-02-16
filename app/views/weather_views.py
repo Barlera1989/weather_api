7344f54c28fef0538112fbd8a40e1e42fc54ab6a
rafael <barlera1989@gmail.com>
2021-02-16 14:12:47 -0300

    feature: get weather data

--- app/views/weather_views.py
+++ app/views/weather_views.py
@@ -1,14 +1,17 @@
 from flask import Blueprint, Flask, request
+from app.services.weather_data import get_weather_api_data
 
 bp_all_cities = Blueprint('all_blueprint', __name__)
 bp_city_name = Blueprint('cities_blueprint', __name__)
 
 
-@bp_all_cities.route('/weather/all', methods=["GET"])
-def get():
-    return {'message': 'ok'}
-
-
 @bp_city_name.route('/weather/cities', methods=["GET"])
 def get():
     return {'message': 'done!'}
+
+
+@bp_all_cities.route('/weather/all', methods=["GET"])
+def get():
+    data = get_weather_api_data()
+    print(data.__dict__)
+    return {'message': 'ok'}
